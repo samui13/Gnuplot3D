39c6f6e1b83832e9936ecb2192335a98e5886b70
henna13 <ss.to13@gmail.com>
2013-08-30 06:08:18 +0900

    Capture func

--- process.h
+++ process.h
@@ -1,5 +1,5 @@
 // -*- coding: utf-8 -*-
-// Last-Updated : <2013/08/30 05:50:12 by samui>
+// Last-Updated : <2013/08/30 06:07:50 by samui>
 #ifndef __PROCESS__
 #define __PROCESS__
 #include <OpenGL/gl.h>
@@ -26,6 +26,7 @@ public:
   static void keyfunc(unsigned char key,int x,int y);
   static void keyspecial(int key,int x,int y);
   static void idle();
+  static void capture();
 public:
   void Init();
 private:
@@ -132,6 +133,9 @@ void ProcessGL::keyfunc(unsigned char key,int x,int y){
   case 'q':
     exit(1);
     break;
+  case 'c':
+    capture();
+    break;
   case 'n':
     views.setVC(views.getVC()-1);
     break;
@@ -177,6 +181,57 @@ void ProcessGL::MouseFunc(int button,int stat, int x, int y){
     std::cout<<"H"<<std::endl;
   }
   
+}
+void ProcessGL::capture(){
+  // Copy from here
+  //http://d.hatena.ne.jp/yatt/20091004/1254678048
+
+  const char filepath[] = "./output.png";
+  png_bytep raw1D;
+  png_bytepp raw2D;
+  int i;
+  int width = glutGet(GLUT_WINDOW_WIDTH);
+  int height = glutGet(GLUT_WINDOW_HEIGHT);
+
+  // 構造体確保
+  FILE *fp = fopen(filepath, "wb");
+  png_structp pp = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  png_infop ip = png_create_info_struct(pp);
+  // 書き込み準備
+  png_init_io(pp, fp);
+  png_set_IHDR(pp, ip, width, height,
+	       8, // 8bit以外にするなら変える
+	       PNG_COLOR_TYPE_RGBA, // RGBA以外にするなら変える
+	       PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+  // ピクセル領域確保
+  raw1D = (png_bytep)malloc(height * png_get_rowbytes(pp, ip));
+  raw2D = (png_bytepp)malloc(height * sizeof(png_bytep));
+  for (i = 0; i < height; i++)
+    raw2D[i] = &raw1D[i*png_get_rowbytes(pp, ip)];
+  // 画像のキャプチャ
+  glReadBuffer(GL_FRONT);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // 初期値は4
+  glReadPixels(0, 0, width, height,
+	       GL_RGBA, // RGBA以外にするなら変える
+	       GL_UNSIGNED_BYTE, // 8bit以外にするなら変える
+	       (void*)raw1D);
+  // 上下反転
+  for (i = 0; i < height/ 2; i++){
+    png_bytep swp = raw2D[i];
+    raw2D[i] = raw2D[height - i - 1];
+    raw2D[height - i - 1] = swp;
+  }
+  // 書き込み
+  png_write_info(pp, ip);
+  png_write_image(pp, raw2D);
+  png_write_end(pp, ip);
+  // 開放
+  png_destroy_write_struct(&pp, &ip);
+  fclose(fp);
+  free(raw1D);
+  free(raw2D);
+
+  printf("write out screen capture to '%s'\n", filepath);
 }
 #endif
 
